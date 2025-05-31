3f0827aa288403e2f063dc1ac82c6360fcc2712d
fullstacknyc <jgomezval@icloud.com>
2025-05-31 19:42:47 -0400

    adding my links

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const projects = [
         { name: "myDevGuides", description: "My personal information library on Software Engineering technologies.", link: "https://vscript.vercel.app" },
         { name: "AudioCloud", description: "A music sharing platform for music engineers and artists alike.", link: "https://s3-audio-uploader.vercel.app" },
-        { name: "myLinkedIn", description: "Connect with me professionally.", link: "https://www.linkedin.com/in/camilogomezvalencia" }
+        { name: "myLinkedIn", description: "Connect with me professionally.", link: "https://www.linkedin.com/in/camilogomezvalencia" },
+        { name: "myGitHub", description: "Explore my projects and contributions.", link: "https://www.github.com/fullstacknyc" }
     ];
 
     const projectsContainer = document.getElementById("projects-container");
